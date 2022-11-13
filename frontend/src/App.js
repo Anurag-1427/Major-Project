02a50df88f606b7d5db0f0f7be565c59803d1611
Anurag-1427 <kaushikanurag2001@gmail.com>
2022-11-13 09:50:44 +0530

    redux almost

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,17 @@ import Header from "./components/Header/Header";
 import LandingPage from "./screens/Landing Page/LandingPage";
 import { BrowserRouter, Route } from "react-router-dom";
 import MyNotes from "./screens/MyNotes/MyNotes";
+import LoginScreen from "./screens/LoginScreen/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen/RegisterScreen";
 
 const App = () => (
   <BrowserRouter>
     {/* <Header /> */}
     <main>
       <Route exact path="/" component={LandingPage} />
-      <Route path="/mynotes" component={MyNotes} />
+      <Route exact path="/login" component={LoginScreen} />
+      <Route exact path="/register" component={RegisterScreen} />
+      <Route path="/mynotes" component={() => <MyNotes />} />
     </main>
     <Footer />
   </BrowserRouter>
--- frontend/src/screens/Landing Page/LandingPage.js
+++ frontend/src/screens/Landing Page/LandingPage.js
@@ -1,38 +1,49 @@
-import React from "react";
-import { Container, Row, Button } from "react-bootstrap";
-import "./LandingPage.css";
+import React, { useEffect } from "react";
+import { Button, Container, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import "./LandingStyles.css";
 
-const LandingPage = () => {
+function LandingPage({ history }) {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push("/mynotes");
+    }
+  }, [history, userInfo]);
+
   return (
     <div className="main">
       <Container>
         <Row>
           <div className="intro-text">
             <div>
-              <h1 className="title">Welcome to Profilic</h1>
-              <p className="subtitle">One place for your all tasks</p>
+              <h1 className="title">Welcome to Note Zipper</h1>
+              <p className="subtitle">One Safe place for all your notes.</p>
             </div>
             <div className="buttonContainer">
-              <a href="/login">
+              <Link to="/login">
                 <Button size="lg" className="landingbutton">
                   Login
                 </Button>
-              </a>
-              <a href="/register">
+              </Link>
+              <Link to="/register">
                 <Button
+                  variant="outline-primary"
                   size="lg"
                   className="landingbutton"
-                  variant="outline-primary"
                 >
-                  Sign Up
+                  Signup
                 </Button>
-              </a>
+              </Link>
             </div>
           </div>
         </Row>
       </Container>
     </div>
   );
-};
+}
 
 export default LandingPage;
